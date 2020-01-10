7b95e13735ccdbc7bd40b5977eda789cd34455f4
Matt Corsaro <matthew_corsaro@brown.edu>
2020-01-10 16:41:48 -0500

    Added bash function to save images published by our sensors so we don't have to look up the command.

--- kuka.sh
+++ kuka.sh
@@ -8,3 +8,8 @@ export ROS_IP=138.16.161.135
 export ROS_MASTER_URI=http://$ROS_IP:11311
 
 source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/kuka_cam.sh
+
+function save_cam_images()
+{
+	rosrun image_view extract_images _sec_per_frame:=0.01 image:=/cam_$1/color/image_raw
+}
