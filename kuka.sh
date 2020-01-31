7cd2c466b3029ff7c21cc49938a1840bcb277b03
Matt Corsaro <matthew_corsaro@brown.edu>
2020-01-31 13:28:07 -0500

    alias to save depth image.

--- kuka.sh
+++ kuka.sh
@@ -11,10 +11,15 @@ source $KUKA_WS_PATH/src/kuka_brown/kuka_cam/kuka_cam.sh
 
 function save_cam_images()
 {
-	rosrun image_view extract_images _sec_per_frame:=0.01 image:=/cam_$1/color/image_raw
+    rosrun image_view extract_images _sec_per_frame:=0.01 image:=/cam_$1/color/image_raw
+}
+
+function save_depth_images()
+{
+    rosrun image_view image_saver image:=/cam_$1/aligned_depth_to_color/image_raw _encoding:=16UC1 _filename_format:="image%04i.png"
 }
 
 function save_cam_clouds()
 {
-	rosrun pcl_ros pointcloud_to_pcd input:=/cam_$1/depth/color/points
+    rosrun pcl_ros pointcloud_to_pcd input:=/cam_$1/depth/color/points
 }
